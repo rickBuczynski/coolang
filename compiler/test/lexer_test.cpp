af8b76733cc35512f5e74c01472646bc7220537d
Rick Buczynski <rick.buczynski@gmail.com>
2018-05-23 20:36:09 -0400

    refactor test output printing

--- compiler/test/lexer_test.cpp
+++ compiler/test/lexer_test.cpp
@@ -13,14 +13,22 @@ std::string get_expected_output(std::string expected_output_file) {
   return buffer.str();
 }
 
-TEST(LexerTest, AllElseTrue) {
-  Lexer lexer(LEXER_TEST_DATA_PATH "/all_else_true.cl.cool");
+std::string get_lexer_output(std::string input_file_name) {
+  Lexer lexer(LEXER_TEST_DATA_PATH + input_file_name);
   Token tok;
 
-  while ((tok = lexer.GetNextToken()).token_type != TokenType::END_OF_FILE) {
-    std::cout << tok.ToString() << std::endl;
-  }
+  std::string lexer_output = "#name \"" + input_file_name + "\"" + '\n';
 
+  while ((tok = lexer.GetNextToken()).token_type != TokenType::END_OF_FILE) {
+    lexer_output += tok.ToString();
+    lexer_output += '\n';
+  }
+  return lexer_output;
+}
+
+TEST(LexerTest, AllElseTrue) {
+  std::string lexer_output = get_lexer_output("all_else_true.cl.cool");
+  std::cout << lexer_output;
   std::cout << get_expected_output(LEXER_TEST_DATA_PATH
                                    "/all_else_true.cl.cool.out");
   EXPECT_EQ(1 + 1, 2);
