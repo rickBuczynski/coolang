47759da723b2bde6cbf3d9548737b62e2fd6d798
Rick Buczynski <rick.buczynski@gmail.com>
2018-05-25 01:06:03 -0400

    add TODOs

--- compiler/src/char_stream.cpp
+++ compiler/src/char_stream.cpp
@@ -8,9 +8,11 @@ CharStream::CharStream(std::string input_file_name) : infile(input_file_name) {
     std::exit(1);
   }
   // get the first char ready
+  // TODO might be easier to do first Pop in lexer then don't need to pop before every lexer return
   Pop();
 }
 
+// TODO make Pop void like http://www.cplusplus.com/reference/queue/queue/pop/
 char CharStream::Pop() {
   if (index_in_line >= cur_line.length()) {
     if (infile.eof()) {
