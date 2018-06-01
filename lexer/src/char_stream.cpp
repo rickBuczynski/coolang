84c45d1fef58f67be152ea0748e7d6c5e78f782f
Rick Buczynski <rick.buczynski@gmail.com>
2018-05-31 21:31:00 -0400

    Open file as binary so ASCII code 26 doesn't cause EOF before real EOF

--- lexer/src/char_stream.cpp
+++ lexer/src/char_stream.cpp
@@ -2,7 +2,10 @@
 #include <iostream>
 #include "coolang/lexer/char_stream.h"
 
-CharStream::CharStream(std::string input_file_name) : infile(input_file_name) {
+CharStream::CharStream(std::string input_file_name)
+    // open as binary to prevent ascii code 26 from triggering EOF
+    // https://stackoverflow.com/questions/15034012/ifstream-eof-end-of-file-is-reached-before-the-real-end
+    : infile(input_file_name, std::ios_base::in | std::ios_base::binary) {
   if (infile.fail()) {
     std::cerr << "Failed to open file: " << input_file_name << std::endl;
     std::exit(1);
