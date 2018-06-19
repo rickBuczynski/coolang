283e9e57c5834c6ff23e393d8eeca8bfa3135004
Rick Buczynski <rick.buczynski@gmail.com>
2018-06-18 22:16:26 -0400

    Add all parser tests.

--- parser/test/parser_test.cpp
+++ parser/test/parser_test.cpp
@@ -49,31 +49,15 @@ void TestParser(const std::string& input_file) {
   EXPECT_EQ(expected_output, parser_output);
 }
 
-TEST(ParserTest, classonefield) { TestParser("classonefield.test"); }
-TEST(ParserTest, classnoname) { TestParser("classnoname.test"); }
-TEST(ParserTest, classtwofields) { TestParser("classtwofields.test"); }
-TEST(ParserTest, assignment) { TestParser("assignment.test"); }
 TEST(ParserTest, addedlet) { TestParser("addedlet.test"); }
 TEST(ParserTest, arithprecedence) { TestParser("arithprecedence.test"); }
-TEST(ParserTest, associativityminus) { TestParser("associativity-.test"); }
 TEST(ParserTest, assigngetstype) { TestParser("assigngetstype.test"); }
+TEST(ParserTest, assignment) { TestParser("assignment.test"); }
 TEST(ParserTest, assignseq) { TestParser("assignseq.test"); }
 TEST(ParserTest, associativityplus) { TestParser("associativity+.test"); }
-TEST(ParserTest, associativitydiv) { TestParser("associativitydiv.test"); }
+TEST(ParserTest, associativityminus) { TestParser("associativity-.test"); }
 TEST(ParserTest, associativitytimes) { TestParser("associativity-times.test"); }
-TEST(ParserTest, ifexpressionblock) { TestParser("ifexpressionblock.test"); }
-TEST(ParserTest, dispatcharglist) { TestParser("dispatcharglist.test"); }
-TEST(ParserTest, dispatchnoargs) { TestParser("dispatchnoargs.test"); }
-TEST(ParserTest, dispatchonearg) { TestParser("dispatchonearg.test"); }
-TEST(ParserTest, letnoinit) { TestParser("letnoinit.test"); }
-TEST(ParserTest, letinit) { TestParser("letinit.test"); }
-TEST(ParserTest, letparens) { TestParser("letparens.test"); }
-TEST(ParserTest, letassociativity) { TestParser("letassociativity.test"); }
-TEST(ParserTest, letinitmultiplebindings) {
-  TestParser("letinitmultiplebindings.test");
-}
-TEST(ParserTest, whileoneexpression) { TestParser("whileoneexpression.test"); }
-
+TEST(ParserTest, associativitydiv) { TestParser("associativitydiv.test"); }
 TEST(ParserTest, atoi) { TestParser("atoi.test"); }
 TEST(ParserTest, attrcapitalname) { TestParser("attrcapitalname.test"); }
 TEST(ParserTest, badblock) { TestParser("badblock.test"); }
@@ -82,8 +66,78 @@ TEST(ParserTest, baddispatch2) { TestParser("baddispatch2.test"); }
 TEST(ParserTest, baddispatch3) { TestParser("baddispatch3.test"); }
 TEST(ParserTest, baddispatch4) { TestParser("baddispatch4.test"); }
 TEST(ParserTest, badexprlist) { TestParser("badexprlist.test"); }
-
+TEST(ParserTest, badfeaturenames) { TestParser("badfeaturenames.test"); }
+TEST(ParserTest, badfeatures) { TestParser("badfeatures.test"); }
 TEST(ParserTest, casemultiplebranch) { TestParser("casemultiplebranch.test"); }
+TEST(ParserTest, casenoexpr) { TestParser("casenoexpr.test"); }
+TEST(ParserTest, classbadinherits) { TestParser("classbadinherits.test"); }
+TEST(ParserTest, classbadname) { TestParser("classbadname.test"); }
+TEST(ParserTest, classmethodonearg) { TestParser("classmethodonearg.test"); }
+TEST(ParserTest, classnoname) { TestParser("classnoname.test"); }
+TEST(ParserTest, classonefield) { TestParser("classonefield.test"); }
+TEST(ParserTest, classtwofields) { TestParser("classtwofields.test"); }
+TEST(ParserTest, comparisonsassoc) { TestParser("comparisons-assoc.test"); }
+TEST(ParserTest, complex) { TestParser("complex.test"); }
+TEST(ParserTest, dispatcharglist) { TestParser("dispatcharglist.test"); }
+TEST(ParserTest, dispatchnoargs) { TestParser("dispatchnoargs.test"); }
+TEST(ParserTest, dispatchonearg) { TestParser("dispatchonearg.test"); }
+TEST(ParserTest, emptyassign) { TestParser("emptyassign.test"); }
+TEST(ParserTest, emptymethodbody) { TestParser("emptymethodbody.test"); }
+TEST(ParserTest, emptyprogram) { TestParser("emptyprogram.test"); }
+TEST(ParserTest, emptystaticdispatch) {
+  TestParser("emptystaticdispatch.test");
+}
+TEST(ParserTest, equalsassociativity) {
+  TestParser("equalsassociativity.test");
+}
+TEST(ParserTest, extrasemicolonblock) {
+  TestParser("extrasemicolonblock.test");
+}
+TEST(ParserTest, firstbindingerrored) {
+  TestParser("firstbindingerrored.test");
+}
+TEST(ParserTest, firstclasserrored) { TestParser("firstclasserrored.test"); }
+TEST(ParserTest, formallists) { TestParser("formallists.test"); }
+TEST(ParserTest, ifexpressionblock) { TestParser("ifexpressionblock.test"); }
 TEST(ParserTest, ifnested) { TestParser("ifnested.test"); }
+TEST(ParserTest, ifnoelse) { TestParser("ifnoelse.test"); }
+TEST(ParserTest, ifnoelsebranch) { TestParser("ifnoelsebranch.test"); }
+TEST(ParserTest, ifnofi) { TestParser("ifnofi.test"); }
+TEST(ParserTest, ifnothenbranch) { TestParser("ifnothenbranch.test"); }
+TEST(ParserTest, isvoidbadtype) { TestParser("isvoidbadtype.test"); }
+TEST(ParserTest, letassociativity) { TestParser("letassociativity.test"); }
+TEST(ParserTest, letinit) { TestParser("letinit.test"); }
+TEST(ParserTest, letinitmultiplebindings) {
+  TestParser("letinitmultiplebindings.test");
+}
+TEST(ParserTest, letnoinit) { TestParser("letnoinit.test"); }
+TEST(ParserTest, letparens) { TestParser("letparens.test"); }
+TEST(ParserTest, lteassociativity) { TestParser("lteassociativity.test"); }
+TEST(ParserTest, multipleatdispatches) {
+  TestParser("multipleatdispatches.test");
+}
+TEST(ParserTest, multipleattributes) { TestParser("multipleattributes.test"); }
+TEST(ParserTest, multipleclasses) { TestParser("multipleclasses.test"); }
+TEST(ParserTest, multipledispatches) { TestParser("multipledispatches.test"); }
+TEST(ParserTest, multiplemethoderrors) {
+  TestParser("multiplemethoderrors.test");
+}
+TEST(ParserTest, nestedblocks) { TestParser("nestedblocks.test"); }
+TEST(ParserTest, nestedlet) { TestParser("nestedlet.test"); }
+TEST(ParserTest, newbadtype) { TestParser("newbadtype.test"); }
+TEST(ParserTest, returntypebad) { TestParser("returntypebad.test"); }
+TEST(ParserTest, secondbindingerrored) {
+  TestParser("secondbindingerrored.test");
+}
+TEST(ParserTest, staticdispatchnoargs) {
+  TestParser("staticdispatchnoargs.test");
+}
+TEST(ParserTest, unaryassociativity) { TestParser("unaryassociativity.test"); }
+TEST(ParserTest, whiletest) { TestParser("while.test"); }
+TEST(ParserTest, whileexpressionblock) {
+  TestParser("whileexpressionblock.test");
+}
+TEST(ParserTest, whilenoloop) { TestParser("whilenoloop.test"); }
+TEST(ParserTest, whileoneexpression) { TestParser("whileoneexpression.test"); }
 
 }  // namespace
