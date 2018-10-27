9b6d0487de7ab94813ca716222eea63cdefe4718
Rick Buczynski <rick.buczynski@gmail.com>
2018-10-27 16:42:16 -0400

    Move platform specific code to platform.h

---
+++ codegen/include/coolang/codegen/platform.h
@@ -0,0 +1,86 @@
+#ifndef COOLANG_CODEGEN_PLATFORM_H
+#define COOLANG_CODEGEN_PLATFORM_H
+
+#include <filesystem>
+
+namespace coolang {
+namespace platform {
+
+inline std::string GetObjectFileExtension() {
+#ifdef _WIN32
+  return ".obj";
+#endif
+
+#ifdef linux
+  return ".o";
+#endif
+}
+
+inline std::string GetExeFileExtension() {
+#ifdef _WIN32
+  return ".exe";
+#endif
+
+#ifdef linux
+  return "";
+#endif
+}
+
+inline std::string GetLinkerCommandWindows(
+    const std::filesystem::path& object_file_path,
+    const std::filesystem::path& exe_file_path) {
+  std::string obj_input_linker_arg = object_file_path.string();
+  obj_input_linker_arg += " ";
+
+  std::string output_exe_linker_arg = "-OUT:";
+  output_exe_linker_arg += exe_file_path.string();
+  output_exe_linker_arg += " ";
+
+  // clang-format off
+  std::string linker_cmd = "cmd /C \"";
+  linker_cmd += "\"C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.14.26428/bin/Hostx86/x86/link.exe\" ";
+  linker_cmd += obj_input_linker_arg;
+  linker_cmd += output_exe_linker_arg;
+  linker_cmd += "libcmt.lib ";
+  // TODO windows requires legacy_stdio_definitions.lib in new versions of MSVC to link against printf but not in old versions
+  // https://github.com/rust-lang/rust/issues/42830
+  linker_cmd += "legacy_stdio_definitions.lib ";
+  linker_cmd += "-LIBPATH:\"C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.14.26428/lib/x86\" ";
+  linker_cmd += "-LIBPATH:\"C:/Program Files (x86)/Windows Kits/10/Lib/10.0.16299.0/um/x86\" ";
+  linker_cmd += "-LIBPATH:\"C:/Program Files (x86)/Windows Kits/10/Lib/10.0.16299.0/ucrt/x86\" ";
+  linker_cmd += "-SUBSYSTEM:CONSOLE ";
+  linker_cmd += "-MACHINE:X86";
+  linker_cmd += "\"";
+
+  return linker_cmd;
+}
+
+inline std::string GetLinkerCommandLinux(
+    const std::filesystem::path& object_file_path,
+    const std::filesystem::path& exe_file_path) { return "TODO"; }
+
+inline std::string GetLinkerCommand(
+    const std::filesystem::path& input_file_path,
+    const std::optional<std::string>& exe_filename) {
+  std::filesystem::path object_file_path = input_file_path;
+  object_file_path.replace_extension(platform::GetObjectFileExtension());
+
+  std::filesystem::path exe_file_path = input_file_path;
+  if (exe_filename.has_value()) {
+    exe_file_path.replace_filename(exe_filename.value());
+  }
+  exe_file_path.replace_extension(platform::GetExeFileExtension());
+
+#ifdef _WIN32
+  return GetLinkerCommandWindows(object_file_path, exe_file_path);
+#endif
+
+#ifdef linux
+  return GetLinkerCommandLinux(object_file_path, exe_file_path);
+#endif
+}
+
+}  // namespace platform
+}  // namespace coolang
+
+#endif  // COOLANG_CODEGEN_PLATFORM_H
--- codegen/src/codegen.cpp
+++ codegen/src/codegen.cpp
@@ -5,6 +5,7 @@
 #include "coolang/codegen/c_std.h"
 #include "coolang/codegen/io_codegen.h"
 #include "coolang/codegen/object_codegen.h"
+#include "coolang/codegen/platform.h"
 #include "coolang/codegen/string_codegen.h"
 #include "coolang/codegen/vtable.h"
 #include "llvm/ADT/APInt.h"
@@ -1057,7 +1058,7 @@ void CodegenVisitor::Visit(const ProgramAst& prog) {
   std::error_code ec;
 
   std::filesystem::path object_file_path = GetProgramAst()->GetFilePath();
-  object_file_path.replace_extension(".obj");
+  object_file_path.replace_extension(platform::GetObjectFileExtension());
 
   llvm::raw_fd_ostream dest(object_file_path.string(), ec,
                             llvm::sys::fs::F_None);
@@ -1089,40 +1090,8 @@ void Codegen::GenerateCode() const {
 }
 
 void Codegen::Link(const std::optional<std::string>& exe_filename) const {
-  std::filesystem::path object_file_path = ast_->GetFilePath();
-  object_file_path.replace_extension(".obj");
-
-  std::string obj_input_linker_arg = object_file_path.string();
-  obj_input_linker_arg += " ";
-
-  std::filesystem::path exe_file_path = ast_->GetFilePath();
-
-  if (exe_filename.has_value()) {
-    exe_file_path.replace_filename(exe_filename.value());
-  }
-
-  exe_file_path.replace_extension(".exe");
-
-  std::string output_exe_linker_arg = "-OUT:";
-  output_exe_linker_arg += exe_file_path.string();
-  output_exe_linker_arg += " ";
-
-  // clang-format off
-  std::string linker_cmd = "cmd /C \"";
-  linker_cmd += "\"C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.14.26428/bin/Hostx86/x86/link.exe\" ";
-  linker_cmd += obj_input_linker_arg;
-  linker_cmd += output_exe_linker_arg;
-  linker_cmd += "libcmt.lib ";
-  // TODO windows requires legacy_stdio_definitions.lib in new versions of MSVC to link against printf but not in old versions
-  // https://github.com/rust-lang/rust/issues/42830
-  linker_cmd += "legacy_stdio_definitions.lib ";
-  linker_cmd += "-LIBPATH:\"C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.14.26428/lib/x86\" ";
-  linker_cmd += "-LIBPATH:\"C:/Program Files (x86)/Windows Kits/10/Lib/10.0.16299.0/um/x86\" ";
-  linker_cmd += "-LIBPATH:\"C:/Program Files (x86)/Windows Kits/10/Lib/10.0.16299.0/ucrt/x86\" ";
-  linker_cmd += "-SUBSYSTEM:CONSOLE ";
-  linker_cmd += "-MACHINE:X86";
-  linker_cmd += "\"";
-  // clang-format on
+  std::string linker_cmd =
+      platform::GetLinkerCommand(ast_->GetFilePath(), exe_filename);
   std::cout << linker_cmd << std::endl;
   system(linker_cmd.c_str());
 }
